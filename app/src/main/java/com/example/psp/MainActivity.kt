2495863eae4eab15f8a64dad92755ef2e7982119
albertoag128 <91797204+albertoag128@users.noreply.github.com>
2022-10-07 16:13:13 +0200

    Retrofit exercise

--- app/src/main/java/com/example/psp/MainActivity.kt
+++ app/src/main/java/com/example/psp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.psp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.psp.ut02.data.remote.RetroFitApiClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apiClient = RetroFitApiClient()
+
+        Thread{
+            val users = apiClient.getUsers()
+            Log.d("@dev", "Usuarios $users")
+        }.start()
+
+        Thread{
+            val user = apiClient.getUser(1)
+            Log.d("@dev", "Usuario $user")
+        }.start()
     }
 }
